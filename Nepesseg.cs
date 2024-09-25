4d0213f3437cf91f963798cc7d5568551637089a
KDM <konczd@kkszki.hu>
2024-09-25 18:39:45 +0200

    beolvasas:kesz

---
+++ Nepesseg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nepesseg
+{
+    public class Nepesseg
+    {
+        public string orszag;
+        public int terulet;
+        public int nepesseg;
+        public string fovaros;
+        public int fo_nepesseg;
+        public Nepesseg(string s)
+        {
+            string[] darab = s.Split(';');
+            if (darab[2].Contains('g'))
+            {
+                int valt = int.Parse(darab[2].Replace('g', ' '));
+                this.orszag = darab[0];
+                this.terulet = int.Parse(darab[1]);
+                this.nepesseg = valt * 10000;
+                this.fovaros = darab[3];
+                this.fo_nepesseg = int.Parse(darab[4]);
+            }
+            else
+            {
+                this.orszag = darab[0];
+                this.terulet = int.Parse(darab[1]);
+                this.nepesseg = int.Parse(darab[2]);
+                this.fovaros = darab[3];
+                this.fo_nepesseg = int.Parse(darab[4]);
+            }
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,15 @@ namespace nepesseg
     {
         static void Main(string[] args)
         {
+            List<Nepesseg> neplist = new List<Nepesseg>();
+            StreamReader sr = new StreamReader("adatok-utf8.txt");
+            string headline=sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                neplist.Add(new Nepesseg(sr.ReadLine()));
+            }
+            sr.Close();
+            Console.ReadLine();
         }
     }
 }
